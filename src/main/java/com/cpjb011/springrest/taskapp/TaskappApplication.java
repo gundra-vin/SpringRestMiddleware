cdcfe95565d88d9a22ac4840d2292bb38cee12b5
vinoth kumar <gundra.vinoth87@gmail.com>
2019-03-15 17:36:21 +0530

    First Commit

---
+++ src/main/java/com/cpjb011/springrest/taskapp/TaskappApplication.java
@@ -0,0 +1,17 @@
+package com.cpjb011.springrest.taskapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class TaskappApplication {
+
+	
+	public static void main(String[] args) {
+		SpringApplication.run(TaskappApplication.class, args);
+	}
+
+}
+
